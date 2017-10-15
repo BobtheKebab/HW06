d92e769d6a42f7c87e45ea5037e7802fff6ec219
BobtheKebab <ahbab.ashraf@gmail.com>
2017-10-15 17:16:53 -0400

    Should be done

--- HW06.c
+++ HW06.c
@@ -14,9 +14,38 @@ void print_list (struct node *list) {
   }
 }
 
+struct node * insert_front (struct node *list, int data) {
+  struct node *insert = (struct node *) malloc(sizeof(struct node));
+  (*insert).num = data;
+  (*insert).next = list;
+  return insert;
+}
+
+struct node * free_list (struct node *list) {
+  struct node *front = list;
+  while (list) {
+    struct node *remove = list;
+    list = (*list).next;
+    free(remove);
+  }
+  return front;
+}
+
 int main () {
 
-  printf("boi\n");
+  struct node *list = 0;
+  int count = 5;
+  while (count > -1) {
+    list = insert_front(list, count * 10);
+    count--;
+  }
+
+  printf("Printing the list\n");
+  print_list(list);
+  printf("Now freeing the list\n");
+  free_list(list);
+  printf("Printing again should cause problems\n");
+  print_list(list);
 
   return 0;
 }
